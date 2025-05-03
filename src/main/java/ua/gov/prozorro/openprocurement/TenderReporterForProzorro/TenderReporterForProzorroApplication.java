292418864d0d929b5455da6109c5b2492afa832d
shramkoaleksey90 <shramkoaleksey90@gmail.com>
2025-05-03 16:26:42 +0300

    - initial commit

---
+++ src/main/java/ua/gov/prozorro/openprocurement/TenderReporterForProzorro/TenderReporterForProzorroApplication.java
@@ -0,0 +1,13 @@
+package ua.gov.prozorro.openprocurement.TenderReporterForProzorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderReporterForProzorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderReporterForProzorroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/gov/prozorro/openprocurement/TenderReporterForProzorro/TenderReporterForProzorroApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.gov.prozorro.openprocurement.TenderReporterForProzorro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderReporterForProzorroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
